f5ab8a22d60078c526462e92b7df183ad146d068
lgrega <lisa.marie.grega@gmail.com>
2017-01-26 10:11:08 -0800

    follow up for Lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,27 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("Javascript is connected");
+		$("#testjs").text("Nice one!");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) {
+	console.log("Clicked");
+	//prevent page from reloading
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.toggle();
+    }
+}
+
